34a71d5008267a610588dd6695ff70b8ce01eb48
Anmol1141 <32287456+anmol1141@users.noreply.github.com>
2019-03-12 11:34:58 +0530

    Add files via upload

---
+++ Calculator/calc.js
@@ -0,0 +1,16 @@
+var multiplication = function(a,b){
+  return `the product of number is ${a*b}`;
+}
+var subtract = function(a,b){
+  return `the differnce of number is ${a-b}`;
+}
+var add = function(a,b){
+  return `the addition of number is ${a+b}`;
+}
+var divide = function(a,b){
+  return `the division of number is ${a/b}`;
+}
+module.exports.multiplication=multiplication;
+module.exports.subtract=subtract;
+module.exports.add=add;
+module.exports.divide=divide;
---
+++ Calculator/calcMain.js
@@ -0,0 +1,5 @@
+var calc= require('./calc');
+console.log(calc.multiplication(3,5));
+console.log(calc.subtract(3,5));
+console.log(calc.add(3,5));
+console.log(calc.divide(3,5));
